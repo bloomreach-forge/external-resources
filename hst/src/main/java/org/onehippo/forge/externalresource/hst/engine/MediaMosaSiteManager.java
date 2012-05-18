133c43efca4d3d084f3ec5f0ce06fb1cf42710ee
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2012-05-18 11:22:43 +0000

    avoid nullpointer on check if no assetid present

---
+++ hst/src/main/java/org/onehippo/forge/externalresource/hst/engine/MediaMosaSiteManager.java
@@ -0,0 +1,20 @@
+package org.onehippo.forge.externalresource.hst.engine;
+
+import javax.jcr.Node;
+
+import org.onehippo.forge.externalresource.api.Embeddable;
+import org.onehippo.forge.externalresource.api.utils.NodePluginConfig;
+
+/**
+ * Created with IntelliJ IDEA. User: jhoffman Date: 18-5-12 Time: 13:08 To change this template use File | Settings |
+ * File Templates.
+ */
+public class MediaMosaSiteManager implements Embeddable {
+
+    public MediaMosaSiteManager(Node config) {
+    }
+
+    public String getEmbedded(final Node node) {
+        return null;
+    }
+}
