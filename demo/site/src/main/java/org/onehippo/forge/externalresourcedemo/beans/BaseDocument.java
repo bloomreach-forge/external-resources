a973fb999766377ad2197a763b769ac39e820796
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2017-03-21 10:30:49 +0100

    HIPFORGE-75: do the aftermath of moving to GitHub, including documentation, and adding the demo _as is_ from separate SVN repository

---
+++ demo/site/src/main/java/org/onehippo/forge/externalresourcedemo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.externalresourcedemo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="externalresourcedemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
