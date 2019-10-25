e57fb21824545676c4dd45365aa9921d5fd7b74d
Joost Oudeman <joost@openweb.nl>
2019-10-25 16:29:01 +0200

    Use esv2yaml tool

--- demo/site/src/main/java/org/onehippo/forge/externalresourcedemo/beans/BaseDocument.java
+++
@@ -1,9 +0,0 @@
-package org.onehippo.forge.externalresourcedemo.beans;
-
-import org.hippoecm.hst.content.beans.Node;
-import org.hippoecm.hst.content.beans.standard.HippoDocument;
-
-@Node(jcrType="demo:basedocument")
-public class BaseDocument extends HippoDocument {
-
-}
